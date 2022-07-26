8fcd3d925de77b09a9e94f6eaf7644592a52792d
Mukhriddin Badriev <muhriddinbadriyev7@gmail.com>
2022-07-26 09:45:31 +0500

    F.S and A

--- 3.5.1.cpp
+++ 3.5.1.cpp
@@ -4,50 +4,57 @@ using namespace std;
 class Figure {
 protected:
 	int sides_count=0;
-	string name="Фигура";
+	string name;
 public:
+	Figure(){}
 	int get_sides_count() {
 		return sides_count;
 	}
 	string get_name() {
 		return name;
+	}
+	Figure(string new_name,int new_sides_count) {
+		name = new_name;
+		sides_count = new_sides_count;
 	}
 };
 
 class Triangle:public Figure {
 public:
 	int get_sides_count() {
-		sides_count = 3;
 		return sides_count;
 	}
 	string get_name() {
-		name = "Треугольник";
 		return name;
+	}
+	Triangle(string name1,int a) :Figure() {
+		name = name1;
+		sides_count = a;
 	}
 };
 
 class Quadrangle:public Figure {
 public:
 	int get_sides_count() {
-		sides_count = 4;
 		return sides_count;
 	}
 	string get_name() {
-		name = "Четырёхугольник";
 		return name;
+	}
+	Quadrangle(string name1, int a) :Figure() {
+		name = name1;
+		sides_count = a;
 	}
 };
 
 int main() {
 	setlocale(LC_ALL, "rus");
-	Figure figure;
-	Triangle triangle;
-	Quadrangle quadrangle;
-
+	Figure figure("Фигура",0);
+	Triangle triangle("Треугольник",3);
+	Quadrangle quadrangle("Четырёхугольник",4);
 	cout << "Количество сторон:" << endl;
 	cout << figure.get_name() << ": " << figure.get_sides_count() << endl;
 	cout << triangle.get_name() << ": " << triangle.get_sides_count() << endl;
 	cout << quadrangle.get_name() << ": " << quadrangle.get_sides_count() << endl;
-
 	return 0;
 }
